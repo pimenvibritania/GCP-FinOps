b9adf48c37fc3403deabaee2d35d1da3a774e17a
Maulana Ibrahim <maulana.ibrahim@moladin.com>
2023-08-18 12:00:31 +0800

    add try catch and slack alert

--- api/cron.py
+++ api/cron.py
@@ -1,6 +1,21 @@
 import datetime
 import requests
+import json
+import logging
 from os import getenv
+
+logging.basicConfig(level=logging.ERROR)
+logger = logging.getLogger(__name__)
+
+def send_slack(message):
+    SLACK_WEBHOOK_URL = getenv('SLACK_WEBHOOK_URL')
+
+    headers = {"Content-type": "application/json"}
+    payload = {"text": message}
+    try:
+        requests.post(SLACK_WEBHOOK_URL, headers=headers, data=json.dumps(payload))
+    except requests.exceptions.RequestException as e:
+        logger.error("Send Slack Error: %s", e)
 
 
 def insert_kubecost_data():
@@ -8,14 +23,22 @@ def insert_kubecost_data():
     KUBECOST_CRONJOB_PASSWORD = getenv('KUBECOST_CRONJOB_PASSWORD')
 
     current_datetime = datetime.datetime.now()
-    yesterday_datetime = current_datetime - datetime.timedelta(days=1)
+    yesterday_datetime = current_datetime - datetime.timedelta(days=2)
     yesterday = yesterday_datetime.strftime("%Y-%m-%d")
 
     url = 'https://cost-management.moladin.com/api/kubecost/insert-data'
     auth_credentials = (KUBECOST_CRONJOB_USER, KUBECOST_CRONJOB_PASSWORD)
-
     date = yesterday
-    response = requests.post(url, json={"date": date}, auth=auth_credentials)
-    print(f"Date {date}, Status Code: {response.status_code}")
-    print("Response:", response.text)
-
+    try:
+        response = requests.post(url, json={"date": date}, auth=auth_credentials)
+        if response.status_code != 200:
+            logs = f"Kubecost Daily Cronjob Failed. Date: {date}, Response: {response.status_code}, {response.text}"
+            logger.error(logs)
+            send_slack(f"<!here> \n*Kubecost Cronjob Failed!* :rotating_light: \nLogs: ```{logs}```")
+            return
+        logs = f"Kubecost Daily Cronjob Success. Response: {response.status_code}, {response.text}"
+        logger.info(logs)
+        send_slack(f"<!here> \n*Kubecost Cronjob Success!* :white_check_mark: \nLogs: ```{logs}```")
+    except requests.exceptions.RequestException as e:
+        logger.error("Error: %s", e)
+        send_slack(f"<!here> \n*Kubecost Cronjob Failed!* :rotating_light: \nLogs: ```{e}```")
