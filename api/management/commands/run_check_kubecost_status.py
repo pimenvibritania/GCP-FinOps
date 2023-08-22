35d4aa4a91444401113bf4dd20e5760c362ad154
pirmanabd <pirman.abdurohman@moladin.com>
2023-08-22 14:58:13 +0700

    fix(BEI-1033): Styling PDF

--- api/management/commands/run_check_kubecost_status.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-from api.cron import check_kubecost_status
-
-class Command(BaseCommand):
-    help = 'Execute cron logic directly for testing'
-
-    def handle(self, *args, **options):
-        check_kubecost_status()
