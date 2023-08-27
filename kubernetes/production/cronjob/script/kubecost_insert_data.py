50b62acb0df6a5eedf4cf867598b9949ca967f63
Maulana Ibrahim <maulana.ibrahim@moladin.com>
2023-08-28 01:56:24 +0800

    update slack message kubecost cronjob

--- kubernetes/production/cronjob/script/kubecost_insert_data.py
+++ kubernetes/production/cronjob/script/kubecost_insert_data.py
@@ -8,6 +8,7 @@ from notif import send_slack
 KUBECOST_CRONJOB_USER = getenv("KUBECOST_CRONJOB_USER")
 KUBECOST_CRONJOB_PASSWORD = getenv("KUBECOST_CRONJOB_PASSWORD")
 APP_URL = getenv("APP_URL")
+ENVIRONMENT = getenv("ENVIRONMENT")
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -25,21 +26,15 @@ def insert_kubecost_data():
     try:
         response = requests.post(endpoint, json={"date": date}, auth=auth_credentials)
         if response.status_code != 200:
-            logs = f"{current_datetime} - Kubecost Daily Cronjob Failed. Date: {date}, Response: {response.status_code}, {response.text}"
+            logs = f"{current_datetime} - Kubecost Insert Data Failed. Endpoint: {endpoint}, Response: {response.status_code}"
             logger.error(logs)
-            send_slack(
-                f"<!here> \n*Kubecost Cronjob Failed!* :rotating_light: \nLogs: ```{logs}```"
-            )
+            send_slack(f"<!here> *Kubecost Insert Data Failed!* :rotating_light:\nEnvironment: *{ENVIRONMENT}*\nLogs: ```{logs}```")
             return
-        logs = f"{current_datetime} - Kubecost Daily Cronjob Success. Response: {response.status_code}, {response.text}"
+        logs = f"{current_datetime} - Kubecost Insert Data Success. Response: {response.status_code}, {response.text}"
         logger.info(logs)
-        send_slack(
-            f"<!here> \n*Kubecost Cronjob Success!* :white_check_mark: \nLogs: ```{logs}```"
-        )
+        send_slack(f"<!here> *Kubecost Insert Data Success!* :white_check_mark:\nEnvironment: *{ENVIRONMENT}*\nLogs: ```{logs}```")
     except requests.exceptions.RequestException as e:
         logger.error("Error: %s", e)
-        send_slack(
-            f"<!here> \n*Kubecost Cronjob Failed!* :rotating_light: \nLogs: ```{e}```"
-        )
+        send_slack(f"<!here> \n*Kubecost Insert Data Failed!* :rotating_light:\nEnvironment: *{ENVIRONMENT}*\nLogs: ```{e}```")
 
 insert_kubecost_data()
