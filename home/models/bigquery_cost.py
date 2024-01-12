fdbea946ede2a4eef0dc347ecf135b7462c04723
pirmanabd <pirman.abdurohman@moladin.com>
2024-01-12 14:20:41 +0700

    feat(BEI-1260): validate

--- home/models/bigquery_cost.py
+++ home/models/bigquery_cost.py
@@ -2,11 +2,19 @@ from django.db import models
 
 from home.models.base_model import BaseModel
 from home.models.bigquery_user import BigqueryUser
+from home.models.gcp_projects import GCPProjects
 
 
 class BigqueryCost(BaseModel):
     class Meta:
         db_table = "bigquery_cost"
+
+        constraints = [
+            models.UniqueConstraint(
+                fields=["usage_date", "bigquery_user"],
+                name="unique_bigquery_cost_daily_usage",
+            )
+        ]
 
     usage_date = models.DateField(blank=False)
     cost = models.FloatField()
@@ -14,6 +22,7 @@ class BigqueryCost(BaseModel):
     bigquery_user = models.ForeignKey(
         BigqueryUser, on_delete=models.PROTECT, blank=False
     )
+    gcp_project = models.ForeignKey(GCPProjects, on_delete=models.PROTECT, blank=False)
     metabase_user = models.CharField(max_length=100, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, auto_now=False, blank=False)
     updated_at = models.DateTimeField(auto_now_add=True, auto_now=False, blank=False)
