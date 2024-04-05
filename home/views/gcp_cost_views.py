4f90ba469a26aad48f8a881be1951e753e1efae9
pirmanabd <pirman.abdurohman@moladin.com>
2024-04-05 09:32:22 +0700

    add total cost

--- home/views/gcp_cost_views.py
+++ home/views/gcp_cost_views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.generic import ListView
 
 from api.models.bigquery import BigQuery
+from api.utils.conversion import Conversion
+from api.utils.date import Date
 from home.models import TechFamily
 from home.utils.decorators import is_authenticated
 
@@ -29,12 +31,31 @@ def cost_report_form(request):
     if input_date is None or input_period is None or input_tf is None:
         services = []
         date_range = None
+        total_current_week = 0
+        total_previous_week = 0
+
     else:
         report = BigQuery.get_periodical_cost(input_date, input_period)
         services = report[input_tf]["data"]["services"]
         date_range = report[input_tf]["data"]["range_date"]
+        total_current_week = report[input_tf]["data"]["summary"]["current_period"]
+        total_previous_week = report[input_tf]["data"]["summary"]["previous_period"]
+
+    (
+        current_period_from,
+        current_period_to,
+        previous_period_from,
+        previous_period_to,
+    ) = Date.get_date_range(input_date, input_period)
 
     tech_family = TechFamily.tech_cost()
+
+    if total_current_week > total_previous_week:
+        icon = "arrow_upward"
+        bg_color = "danger"
+    else:
+        icon = "arrow_downward"
+        bg_color = "primary"
 
     data = {
         "tech_family": tech_family,
@@ -43,6 +64,12 @@ def cost_report_form(request):
         "input_date": input_date,
         "input_period": input_period,
         "input_tf": input_tf,
+        "total_current_week": Conversion.idr_format(total_current_week),
+        "total_previous_week": Conversion.idr_format(total_previous_week),
+        "icon": icon,
+        "bg_color": bg_color,
+        "current_week": f"{current_period_from} - {current_period_to}",
+        "previous_week": f"{previous_period_from} - {previous_period_to}",
     }
 
     return render(request, "pages/gcp_cost_report.html", data)
